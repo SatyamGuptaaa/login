79c0ba76a6516558994bf08d034cd993c699a75d
Satyam Gupta <65031763+SatyamGuptaaa@users.noreply.github.com>
2023-02-09 08:36:43 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBULpAq8NZsy5Bop5gVDJTffyUATo9KeQU",
+    authDomain: "stymrj-465d3.firebaseapp.com",
+    projectId: "stymrj-465d3",
+    storageBucket: "stymrj-465d3.appspot.com",
+    messagingSenderId: "132678768265",
+    appId: "1:132678768265:web:b5f1cd60a39afbe9a2d858",
+    measurementId: "G-DTHYM2SQ3T"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
